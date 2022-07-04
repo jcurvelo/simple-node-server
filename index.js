089b289f6fe34a0da0a47d055193499fdc030c3f
jcurvelo <juancurvelo99@gmail.com>
2022-07-04 15:26:43 -0400

    saludo

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/hola', (req, res) => res.send('Has saludado!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
